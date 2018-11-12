21cf4f02eedc4359d00f804921963c5a81f74857
Noé Canú <ncanug@miumg.edu.gt>
2018-11-12 01:35:37 -0600

    Login functionality added

--- app/login/login.js
+++ app/login/login.js
@@ -9,6 +9,38 @@ angular.module('myApp.login', ['ngRoute'])
         });
     }])
 
-    .controller('loginCtrl', [function() {
+    .controller('loginCtrl', ['$scope','$rootScope','$http','$location', function($scope, $rootScope, $http,$location) {
+
+        $scope.user = {};
+
+
+        $scope.login = function () {
+
+            $http.post("http://localhost:8080/api/user/login", $scope.user).then
+            (
+                function(response)
+                {
+
+                    var token = response.data.data.token;
+                    localStorage.setItem("token", token);
+                    $http.defaults.headers.common.Authorization = "Bearer "+ localStorage.getItem("token");
+
+                    $location.path( '/dashboard' );
+
+
+
+                },
+                function(error){
+                    // console.log($scope.trialCompany);
+                    $rootScope.allow = false;
+                    $scope.errorsFounded = error.data.message;
+                    alert($scope.errorsFounded);
+
+                }
+            );
+
+
+        };
+
 
     }]);
