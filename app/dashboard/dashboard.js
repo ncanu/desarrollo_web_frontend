e60888d5378d714c84e83fa1a5c774cae90e776a
Noé Canú <ncanug@miumg.edu.gt>
2018-11-12 23:43:47 -0600

    Multiple changes on dashboard

--- app/dashboard/dashboard.js
+++ app/dashboard/dashboard.js
@@ -9,6 +9,35 @@ angular.module('myApp.dashboard', ['ngRoute'])
         });
     }])
 
-    .controller('dashboardCtrl', [function() {
+    .controller('dashboardCtrl', ['$scope','$rootScope','$http','$location', function($scope, $rootScope, $http,$location) {
+       if( localStorage.getItem("token") === null || localStorage.getItem("token")===undefined)
+       {
+           $location.path( '/login' );
+       }
+
+        $http.defaults.headers.common.Authorization = "Bearer "+ localStorage.getItem("token");
+
+        $scope.userPublicatons = [];
+        $scope.getTimeLine = function () {
+
+            $http.get("http://localhost:8080/api/images/images").then
+            (
+                function(response)
+                {
+                    $scope.userPublicatons = response.data.data;
+
+                },
+                function(error){
+
+                    $scope.errorsFounded = error.data.message;
+                    alert($scope.errorsFounded);
+
+                }
+            );
+
+
+        };
+
+        $scope.getTimeLine();
 
     }]);
